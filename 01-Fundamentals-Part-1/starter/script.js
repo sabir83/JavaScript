bcc02b16196f9dc2e535167d96feedb1a810d4fd
Sabir Vardarlı <sabirvardarli83@gmail.com>
2023-07-04 18:21:02 -0400

    JavaScript first codes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Sabir";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415;
+
+let myFirstJob = 'Logistics';
+let myCurrentJob = 'Tester';//more discriptive instead of
+let job1 = 'Logistics';
+let job2 = 'Tester';
+
+console.log(myFirstJob);
