7c7a43a5dfa2dcf2b76ba8851e8af848b6c074bf
Sabir Vardarlı <sabirvardarli83@gmail.com>
2023-07-10 09:13:16 -0400

    added new scripts

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,81 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;// if we make a miastake when typing use strict will show this error on console.
+if (hasDriversLicense) console.log('I can drive');
+
+// const private = 'Audio';//reserved word used error.
+
+
+// FUNCTION -(Method id Java)
+function logger() {
+    console.log('My name is Sabir');
+}
+
+// calling / runnning / invoking function
+logger();//runs 2 times with buttom
+logger();
+
+
+// apples and oranges are arguments
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// or
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+// Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1983);
+
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1983);
+
+console.log(age1, age2);
+
+
+
+// Arrow function - more shorter and better way of function expression
+const calcAge4 = birthYear => 2037 - birthYear;
+const age4 = calcAge4(1983);
+console.log(age4);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1983, 'Sabir'));
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
